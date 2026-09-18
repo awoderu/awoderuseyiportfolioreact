4b9e3b6879ac17a1dbae9ab4002366fe6855c4d0
unknown <awoderuoseyi@gmail.com>
2026-09-19 00:34:55 +0100

    Fix SubHeading

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,6 @@
-import React from "react";
+
 import Hero from "../components/Hero";
 import ProjectPage from "./projects/projectpage"; 
-import SubHeading from "../components/SubHeading";
 
 const Home = () => {
   return (
